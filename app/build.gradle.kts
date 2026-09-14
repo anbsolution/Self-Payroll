360b8b083a046284a88f5dca072ab22475caebd0
anbsolution <anbsolution@users.noreply.github.com>
2026-09-14 06:33:34 +0530

    Fix Fragment ViewModels dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.fragment:fragment-ktx:1.9.0")
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.activity:activity-ktx:1.11.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
